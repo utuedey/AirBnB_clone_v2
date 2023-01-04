721f99f3325eb83ee02a4823d72c81306e2bb703
utuedey <utuedeyejosph@gmail.com>
2023-01-04 11:57:01 -0800

    modify setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,9 +2,7 @@
 -- create a database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- grant SELECT privileges on performance_schema
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
