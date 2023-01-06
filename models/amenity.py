007b002488c2f6c6ba242aaec93ff82984f11e0a
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2023-01-06 00:01:55 -0800

    Airbnb Clone 2

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 class Amenity(BaseModel):
-    name = ""
+   name = ""
+   __tablename__ = "amenities"
+   name = Column(String(128),
+                 nullable=False)
+   place_amenities = relationship("Place",
+                                  secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,18 +1,75 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+import os
 
-
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id',
+                             String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True,
+                             nullable=False))
 class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+   """ A place to stay """
+   __tablename__ = 'places'
+   city_id = Column(String(60),
+                    ForeignKey('cities.id'),
+                    nullable=False)
+   user_id = Column(String(60),
+                    ForeignKey('users.id'),
+                    nullable=False)
+   name = Column(String(128),
+                 nullable=False)
+   description = Column(String(1024),
+                        nullable=True)
+   number_rooms = Column(Integer,
+                         nullable=False,
+                         default=0)
+   number_bathrooms = Column(Integer,
+                             nullable=False,
+                             default=0)
+   max_guest = Column(Integer,
+                      nullable=False,
+                      default=0)
+   price_by_night = Column(Integer,
+                           nullable=False,
+                           default=0)
+   latitude = Column(Float,
+                     nullable=True)
+   longitude = Column(Float,
+                      nullable=True)
+   reviews = relationship("Review",
+                          backref="place",
+                          cascade="all, delete-orphan")
+   am_id = []
+   if os.getenv('HBNB_TYPE_STORAGE') == 'file':
+       @property
+       def amenities(self):
+           """Returns the instances"""
+           _inst = []
+           for am in am_id:
+               if am.id == self.id:
+                   _inst.append(am)
+           return _inst
+       @amenities.setter
+       def amenities(self, am):
+           """Adds an Amenity"""
+           if type(am).__name__ == 'Amenity':
+               self.am_id.append(am)
+   elif os.getenv('HBNB_TYPE_STORAGE') == 'db':
+       @property
+       def reviews(self):
+           _rev = []
+           for review in self.reviews:
+               if review.place_id == self.id:
+                   _rev.append(review)
+           return(_rev)
+       amenities = relationship("Amenity",
+                                secondary=place_amenity)
