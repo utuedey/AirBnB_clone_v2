79364bfea4724681816c14959956508f6bfa5610
utuedey <utuedeyejosph@gmail.com>
2023-01-09 12:49:49 -0800

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""This is the base model class for AirBnB"""
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import String, DateTime, Column
 import models
-
 
 Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models.
+    """This class will defines all common attributes/methods
+    for other classes
     """
-    id = Column(String(60), primary_key=True, nullable=False)
+    id = Column(String(60), unique=True, nullable=False,
+                primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model.
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
         """
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__" and hasattr(self, key):
+                if key != "__class__":
                     setattr(self, key, value)
             if "id" not in kwargs.keys():
                 setattr(self, "id", str(uuid.uuid4()))
@@ -36,27 +46,26 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a string representation of the instance.
+        """returns a string
+        Return:
+            returns a string of class name, id, and dictionary
         """
-        return '[{}] ({}) {}'.format(
-           type(self).__name__, self.id, self.to_dict)
+        dic = self.to_dict()
+
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, dic)
 
     def __repr__(self):
-        """Return a string representaion.
+        """return a string representaion
         """
         return self.__str__()
 
     def save(self):
-        """Updates updated_at with current time when instance is changed.
+        """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
-
-    def delete(self):
-        """delete the current instance
-        """
-        models.storage.delete(self)
 
     def to_dict(self):
         """creates dictionary of the class  and returns
@@ -70,3 +79,8 @@ class BaseModel:
         if '_sa_instance_state' in my_dict.keys():
             my_dict.pop('_sa_instance_state', None)
         return my_dict
+
+    def delete(self):
+        """Delete the current instance from the storage
+        (models.storage) by calling the method delete"""
+        models.storage.delete(self)
