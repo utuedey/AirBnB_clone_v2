f6bae002eec09faac421c0848fe5480485ba69bc
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2023-01-23 19:26:19 -0800

    Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 #Fabric script that generates a .tgz archive
 
-import os
-from fabric.api import runs, local, sudo
+from fabric.api import runs_once, local, sudo
 from datetime import datetime
 
 n = datetime.now()
 
-
+@runs_once
 def do_pack():
     """ archives contents of the web_static folder"""
-
-     fn = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+    fn = 'versions/web_static_{}{}{}{}{}{}.tgz'\
         .format(n.year, n.month, n.day, n.hour, n.minute, n.second)
     local('mkdir -p versions')
     command = local("tar -cvzf " + fn + " ./web_static/")
     if command.succeeded:
-        return fn
+       return fn
     return None
