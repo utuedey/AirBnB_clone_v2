7e93ed78e4ec5ae39d5645de880d680d929f3543
utuedey <utuedeyejosph@gmail.com>
2023-02-06 11:29:02 -0800

    Add task 1 file

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,11 +10,6 @@ def display_hello_hbnb():
     """returns a string at the root route"""
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Returns a string at the root route."""
-    return "HBNB"
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
