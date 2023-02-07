9001990cef0c0636475a9cede9ecdb286bd0a774
utuedey <utuedeyejosph@gmail.com>
2023-02-07 21:41:29 +0000

    Add task 6 file

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""starts a Flask web application."""
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello_hbnb():
+    """returns a string at the root route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """returns a string at the /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """ Return a string at the /c/text route"""
+    underscore = "_"
+    if underscore in text:
+        return "C " + text.replace(underscore, " ")
+    else:
+        return "C " + text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text):
+    """Return a string at the /python/text route"""
+    underscore = "_"
+    if underscore in text:
+        return "Python " + text.replace(underscore, " ")
+    else:
+        return "Python " + text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def dispay_n_is_number(n):
+    """Return a string at the /number/n route
+    only if n is an integer."""
+    if type(n) == int:
+        return str(n) + " is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_html_page(n):
+    """returns a template at the /number_template/<n> route"""
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slahes=False)
+def display_number_even_odd(n):
+    """Returns a template at the /number_odd_or_even/<n> route."""
+    if type(n) == int:
+        return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
